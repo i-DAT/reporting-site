40438e1701824d2437c3d698b1c825f61833be5d
Christopher Hunt <mail@thisisthechris.co.uk>
2013-04-15 16:32:26 +0100

    Updates requirements to use dev repo on django-registration

--- local-project/urls.py
+++ local-project/urls.py
@@ -11,6 +11,10 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
+    #Registration Module
+    
+    (r'^accounts/', include('registration.backends.default.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
